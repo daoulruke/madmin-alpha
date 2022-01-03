81a7e9c1a42ae9f3cf4ea8b7f21213e9357f8e7b
Christopher <2224402+daoulruke@users.noreply.github.com>
2022-01-04 05:02:38 +1300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -66,7 +66,7 @@ let getRecords = async (url) => {
     const current_path = document.getElementById("current_path");
     if (current_path.childNodes[2]) current_path.removeChild(current_path.childNodes[2]);
     if (current_path.childNodes[3]) current_path.removeChild(current_path.childNodes[3]);
-    const li = document.createElement("li");
+    var li = document.createElement("li");
     li.innerHTML = `<a href="#" onclick="getRecords('${url}')">/${url.split("/")[2]}</a>`;
     current_path.appendChild(li);
 
@@ -93,6 +93,10 @@ let getRecord = async (url) => {
         ul.appendChild(li);
     }
 
+    var li = document.createElement("li");
+    li.innerHTML = `<input type="button" value="EDIT" onclick="editRecord('${url}')" />`;
+    ul.appendChild(li);
+
     document.getElementById('content').innerHTML = ul.outerHTML;
 
     // #raw
@@ -102,7 +106,7 @@ let getRecord = async (url) => {
     // #current_path
     const current_path = document.getElementById("current_path");
     if (current_path.childNodes[3]) current_path.removeChild(current_path.childNodes[3]);
-    const li = document.createElement("li");
+    var li = document.createElement("li");
     li.innerHTML = `<a href="#" onclick="getRecord('${url}')">/${url.split("/")[3]}</a>`;
     current_path.appendChild(li);
 
@@ -114,6 +118,64 @@ let getRecord = async (url) => {
     );
 
 };
+
+
+// Fetch and display records
+let editRecord = async (url) => {
+
+    document.getElementById('content').innerHTML = '<form>';
+
+    const record = await _fetch(`${apiUrl}${url}`);
+
+    // #content
+    const ul = document.createElement("ul");
+
+    for ([key, value] of Object.entries(record)) {
+        const li = document.createElement("li");
+        if(key == 'id') {
+            li.innerHTML = `${key}: ${value}`;
+        } else {
+            li.innerHTML = `<input type="text" name="${key}" value="${value}" />`;
+        }
+        ul.appendChild(li);
+    }
+
+    var li = document.createElement("li");
+    li.innerHTML = `<input type="button" value="SUBMIT" onclick="submitForm('edit_form')" />`;
+    ul.appendChild(li);
+
+    const form = document.createElement("form");
+    form.setAttribute('id', 'edit_form');
+    form.appendChild(ul);
+
+    document.getElementById('content').innerHTML = form.outerHTML;
+
+};
+
+function submitForm(form_id) {
+
+  // Prevent the form from submitting.
+  event.preventDefault();
+
+  // Set url for submission and collect data.
+  const url = apiUrl;
+  const formData = new FormData(document.getElementById(form_id));
+  // Build the data object.
+ // const data = {};
+ // formData.forEach((value, key) => (data[key] = value));
+  // Log the data.
+ // console.log(data);
+
+var json = JSON.stringify(Object.fromEntries(formData));
+
+ var accessToken = localStorage.getItem('accessToken');
+
+  var request = new XMLHttpRequest();
+request.open("PUT", url + '/records/firm/54895');
+request.setRequestHeader('X-Authorization', 'Bearer ' + accessToken);
+request.send(json);
+
+}
 
 window.onload = function () {
 
@@ -127,6 +189,8 @@ window.onload = function () {
     } else {
 
         document.location.hash = '';
+
+        localStorage.setItem('accessToken', accessToken);
 
         // Fetch wrapper with default options
         _fetch = (url, options = {}) => {
