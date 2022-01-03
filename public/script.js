91e061823545e35d1850db5fae5b89eb11fab4a2
John Carlo Ricana <cier.alteir@gmail.com>
2022-01-03 14:43:03 +0800

    Display records/record

--- public/script.js
+++ public/script.js
@@ -4,6 +4,64 @@ var clientId = '7FKABiIsVFrGmquJm5k1kf9MKbt1xw2F'; // client id as defined in ph
 var audience = 'https://api.sitevitals-beta.com'; // api audience as defined in php-api-auth
 var apiUrl = 'https://api.halcyon-beta.com'; // api audience as defined in php-api-auth
 
+// Fetch wrapper
+let _fetch = null;
+
+// Fetch and display openapi
+let getOpenapi = async () => {
+    const openapi = await _fetch(`${apiUrl}/openapi`);
+
+    // #content
+    const ul = document.createElement("ul");
+    for ([key, value] of Object.entries(openapi.paths)) {
+        const li = document.createElement("li");
+        li.innerHTML = `<a href="#" onclick="getRecords('${key}')">${key}</a>`;
+        ul.appendChild(li);
+    }
+    document.getElementById('content').innerHTML = ul.outerHTML;
+
+    // #raw
+    const raw = JSON.stringify(openapi, undefined, 4);
+    document.getElementById('raw').innerHTML = raw;
+};
+
+// Fetch and display records
+let getRecords = async (url) => {
+    const records = await _fetch(`${apiUrl}${url}`)
+        .then(response => response.records);
+
+    // #content
+    const ul = document.createElement("ul");
+    for (record of records) {
+        const li = document.createElement("li");
+        li.innerHTML = `<a href="#" onclick="getRecord('${url + `/${record.id}`}')">${record.name}</a>`;
+        ul.appendChild(li);
+    }
+    document.getElementById('content').innerHTML = ul.outerHTML;
+
+    // #raw
+    const raw = JSON.stringify(records, undefined, 4);
+    document.getElementById('raw').innerHTML = raw;
+};
+
+// Fetch and display records
+let getRecord = async (url) => {
+    const record = await _fetch(`${apiUrl}${url}`);
+
+    // #content
+    const ul = document.createElement("ul");
+    for ([key, value] of Object.entries(record)) {
+        const li = document.createElement("li");
+        li.innerHTML = `${key}: ${value}`;
+        ul.appendChild(li);
+    }
+    document.getElementById('content').innerHTML = ul.outerHTML;
+
+    // #raw
+    const raw = JSON.stringify(record, undefined, 4);
+    document.getElementById('raw').innerHTML = raw;
+};
+
 window.onload = function () {
 
     var match = RegExp('[#&]access_token=([^&]*)').exec(window.location.hash);
@@ -18,7 +76,7 @@ window.onload = function () {
         document.location.hash = '';
 
         // Fetch wrapper with default options
-        var _fetch = (url, options = {}) => {
+        _fetch = (url, options = {}) => {
             return fetch(url, {
                 headers: {
                     "X-Authorization": `Bearer ${accessToken}`
@@ -27,21 +85,7 @@ window.onload = function () {
             }).then(response => response.json());
         };
 
-        _fetch(`${apiUrl}/openapi`)
-            .then(response => {
-                // #content
-                const ul = document.createElement("ul");
-                for ([key, value] of Object.entries(response.paths)) {
-                    const li = document.createElement("li");
-                    li.innerHTML = `<a href="${key}">${key}</a>`;
-                    ul.appendChild(li);
-                }
-                document.getElementById('content').appendChild(ul);
-
-                // #raw
-                const raw = JSON.stringify(data, undefined, 4);
-                document.getElementById('raw').innerHTML = raw;
-            });
+        getOpenapi();
     }
 
 };
