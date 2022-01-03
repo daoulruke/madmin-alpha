74058c670edfc16c535af6c1a8cf11272509e671
Christopher <2224402+daoulruke@users.noreply.github.com>
2022-01-03 17:49:52 +1300

    Updates

---
+++ public/script.js
@@ -0,0 +1,56 @@
+
+var authUrl = 'https://sitevitals-beta.au.auth0.com/authorize'; // url of 'auth.php' from php-api-auth
+var clientId = '7FKABiIsVFrGmquJm5k1kf9MKbt1xw2F'; // client id as defined in php-api-auth
+var audience = 'https://api.sitevitals-beta.com'; // api audience as defined in php-api-auth
+var apiUrl = 'https://api.halcyon-beta.com'; // api audience as defined in php-api-auth
+
+window.onload = function () {
+
+    var match = RegExp('[#&]access_token=([^&]*)').exec(window.location.hash);
+    var accessToken = match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+
+    if (!accessToken) {
+
+        document.location = authUrl+'?audience='+audience+'&response_type=token&client_id='+clientId+'&redirect_uri='+document.location.href;
+
+    } else {
+
+        document.location.hash = '';
+
+        var req = new XMLHttpRequest();
+
+        req.onreadystatechange = function () {
+            if (req.readyState==4) {
+
+                const data = JSON.parse(req.responseText);
+
+                // explorer
+                const paths = data.paths;
+                console.log(paths);
+
+                let ul = document.createElement("ul");
+
+                Object.entries(paths).forEach(([key, value]) => {
+                    let li = document.createElement("li");
+                    li.innerHTML = `<a href="${key}">${key}</a>`;
+                    ul.appendChild(li);
+                });
+
+                document.getElementById('explorer').appendChild(ul);
+
+                //raw
+                const raw = JSON.stringify(data, undefined, 4);
+                document.getElementById('raw').innerHTML = raw;
+
+            }
+        }
+
+        url = apiUrl + '/openapi';
+
+        req.open("GET", url, true);
+        req.setRequestHeader('X-Authorization', 'Bearer ' + accessToken);
+        req.send();
+
+    }
+
+};
