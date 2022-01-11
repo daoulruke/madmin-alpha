f759d8fd68462ea08dcf6dfca6b878597b39aa56
Christopher <2224402+daoulruke@users.noreply.github.com>
2022-01-12 01:31:07 +1300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -27,14 +27,14 @@ let updatePath = (url) => {
     const subjectId = urlSegments[3];
     if (subjectId) {
         var li = document.createElement("li");
-        li.innerHTML = `/<a href="#" onclick="getRecord('${`/records/${subject}s/${subjectId}`}')">${subjectId}</a>`;
+        li.innerHTML = `/<a href="#" onclick="getRecord('${`/records/${subject}/${subjectId}`}')">${subjectId}</a>`;
         current_path.appendChild(li);
     }
 
     const join = urlSegments[4];
     if (join) {
         var li = document.createElement("li");
-        li.innerHTML = `/<a href="#" onclick="getRecords('${`/records/${subject}s/${subjectId}/${join}`}')">${join}</a>`;
+        li.innerHTML = `/<a href="#" onclick="getRecords('${`/records/${subject}/${subjectId}/${join}`}')">${join}</a>`;
         current_path.appendChild(li);
     }
 
